c70730c2fc53fa59e387802901b5b9815704ba7d
Aiden Baker <aidenbak9@gmail.com>
2024-11-18 10:26:26 -0600

    Initial Commit

---
+++ StudyFlip/StudyFlipApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyFlipApp.swift
+//  StudyFlip
+//
+//  Created by Aiden Baker on 11/18/24.
+//
+
+import SwiftUI
+
+@main
+struct StudyFlipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
